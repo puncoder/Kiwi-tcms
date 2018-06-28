e28d4f0dcca6338e15353e17e3c28a8a855134b6
Amit <theamit26@gmail.com>
2018-06-28 22:26:46 +0530

    XMLRPC ip

--- plivo/update_status_from_jenkins.py
+++ plivo/update_status_from_jenkins.py
@@ -9,8 +9,8 @@ from tcms_api.mutable import TestRun, TestCaseRunStatus
 from plivo.tcms_utils import get_max_id
 
 # Login to running KIWI server.
-TCMSXmlrpc('plivo', 'root', 'http://127.0.0.1:8000/xml-rpc/')
-# TCMSXmlrpc('plivo', 'root', 'http://0.0.0.0:80/xml-rpc/')
+# TCMSXmlrpc('plivo', 'root', 'http://127.0.0.1:8000/xml-rpc/')
+TCMSXmlrpc('plivo', 'root', 'http://0.0.0.0:80/xml-rpc/')
 
 # Authenticating Jenkins
 home_dir = os.path.expanduser('~')
